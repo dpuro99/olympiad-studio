be0617a43f61c150830070b6e4c89b4964826e97
David Puro <david.puro@frisch.org>
2026-06-17 01:02:50 -0400

    Fixed Google Auth Login

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,12 @@ export default function App() {
       setAuthLoading(true);
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
-        options: { redirectTo: window.location.origin }
+        options: { 
+          redirectTo: window.location.origin,
+          queryParams: {
+            prompt: 'select_account'
+          }
+        }
       });
       if (error) throw error;
     } catch (err) {
